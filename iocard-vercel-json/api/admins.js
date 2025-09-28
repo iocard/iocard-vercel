ffcc5d29b519e7f0ada35fb7cf615f3c0bd633d1
iocard <daviddossantos463@gmail.com>
2025-09-28 20:38:21 -0300

    Delete iocard-vercel-json directory

--- iocard-vercel-json/api/admins.js
+++
@@ -1,7 +0,0 @@
-const { readJSON } = require('./_data')
-const path = require('path')
-module.exports = (req,res)=>{
-  const admins = readJSON(path.join(__dirname,'..','data','admins.json'),[])
-  res.setHeader('Content-Type','application/json')
-  res.end(JSON.stringify(admins))
-}
